c8a3f99cb2ab78a25785783f01e60fe00f273099
Gregory Eales <gregoeales@aol.com>
2019-04-22 23:38:44 -0700

    Update intro_cirq.py

--- intro_cirq.py
+++ intro_cirq.py
@@ -1,4 +1,24 @@
 import cirq
 
+# define the circuit length
+length = 3
 
-print("Success")
+# define qubits
+qubits = [cirq.GridQubit(i, j) for i in range(length) for j in range(length)]
+print(qubits)
+
+# define quantum circuit
+quantum_circuit = cirq.Circuit
+
+for q in qubits:
+
+	print(cirq.H(q))
+
+	"""
+	if q.row + q.col % 2 == 0:
+		quantum_circuit.append(cirq.H(q))
+	if q.row + q.col % 2 == 1:
+		quantum_circuit.append(cirq.X(q))
+	"""
+
+print(quantum_circuit)
