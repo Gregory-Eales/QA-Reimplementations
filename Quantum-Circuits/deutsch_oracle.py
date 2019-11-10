16caaf878c80fafdbb124d18978def9e83bcaf40
Gregory Eales <gregoeales@aol.com>
2019-11-09 19:05:37 -0800

    Edits

--- Quantum-Circuits/deutsch_oracle.py
+++ Quantum-Circuits/deutsch_oracle.py
@@ -5,8 +5,10 @@ class DeutschOracle(object):
 
     def __init__(self):
 
+        self.circuit = cirq.Circuit()
         self.x_gate = cirq.X
-        self.hadmard_gate = Cirq.H
+        self.hadmard_gate = cirq.H
+        self.measure_gate = cirq.MeasurementGate
 
     def init_qubits(self, length=3):
         q = [cirq.GridQubit(i, j) for i in range(length) for j in range(length)]
