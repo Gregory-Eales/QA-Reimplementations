0ad3b97489a2f3a11c36555d18d62990ef804879
Gregory Eales <gregoeales@aol.com>
2020-06-17 15:50:04 +0100

    Changed to Cirq

--- Bernstein–Vazirani-Algorithm/main.py
+++ Bernstein–Vazirani-Algorithm/main.py
@@ -1,6 +1,20 @@
-import qsharp
-from BernsteinVazirani import GenerateSecretString
+import cirq
+import random
 
 
 
-GenerateSecretString.simulate(N=10)
+def set_io_qubits(num_qubits):
+
+	input_qubits = [cirq.GridQubit(i, 0) for i in range(num_qubits)]
+	output_qubit = cirq.GridQubit(num_qubits, 0)
+
+
+# make bv circuit
+def make_bv_circuit(input_qubits, output_qubits, oracle):
+	pass
+
+
+print(cirq.GridQubit(10, 0))
+
+#ircuit = make_bv_circuit(input_qubits, output_qubits, oracle)
+#simulator = cirq.Simulator()
