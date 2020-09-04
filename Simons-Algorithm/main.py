9db74ac8eae89e3fc2148bb709a8bb4c733f6986
Gregory Eales <33496134+Gregory-Eales@users.noreply.github.com>
2020-09-04 11:29:21 -0700

    Init Simons Algorithm

---
+++ Simons-Algorithm/main.py
@@ -0,0 +1,25 @@
+import cirq
+import random
+
+
+
+def black_box_orcale():
+
+	"""
+		- gaurenteed to be a one to one or a two to one function
+
+		- maps one input to one output or maps two diff inputs to
+		 one output and input1 + input2 = b
+
+	"""
+
+	pass
+
+
+def main():
+
+	pass
+
+
+if __name__ == "__main__":
+	main()
